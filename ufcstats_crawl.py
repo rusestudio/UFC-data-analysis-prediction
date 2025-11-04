84dc73896e927df398f7e5a4b741daf2370fa35d
rusestudio <59683457+rusestudio@users.noreply.github.com>
2025-11-04 22:48:18 +0900

    filter fight

--- ufcstats_crawl.py
+++ ufcstats_crawl.py
@@ -12,11 +12,10 @@ MATCH_URL = f"{BASE_URL}/statistics/events/completed?page="
 
 
 def get_match_links(pages=18, stop_date="January 03, 2015"):
-    """Collect all event match links until a specific date"""
     match_links = []
     for page in range(1, pages + 1):
         url = f"{MATCH_URL}{page}"
-        print(f"📄 Scraping event list page {page}: {url}")
+        print(f"scrape event {page}: {url}")
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -28,7 +27,30 @@ def get_match_links(pages=18, stop_date="January 03, 2015"):
             if link_tag and date_tag:
                 date_text = date_tag.text.strip()
                 if date_text == stop_date:
-                    print(f"🛑 Reached {stop_date} — stopping.")
+                    print(f" {stop_date} -stop.")
                     return match_links
                 match_links.append(link_tag["href"])
     return match_links
+
+def filter_fight_links(match_links):
+    stats_link = []
+
+    for match in tqdm(match_links, desc="Collecting fight links"):
+        response = requests.get(match)
+        soup = BeautifulSoup(response.text, "html.parser")
+        fight_rows = soup.find_all(
+            "tr",
+            class_="b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click",
+        )
+
+        for row in fight_rows:
+            tds = row.find_all("td")
+            if len(tds) >= 9:
+                weight_class = tds[6].find("p").get_text(strip=True)
+                method = tds[7].find_all("p")[0].get_text(strip=True)
+                if weight_class == "Lightweight" and method in ["KO/TKO", "KO", "TKO"]:
+                    data_link = row.get("data-link")
+                    stats_link.append(data_link)
+
+    print(f"{len(stats_link)} filtered fight")
+    return stats_link
